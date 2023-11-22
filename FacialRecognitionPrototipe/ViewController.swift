949d41076fa447cf9c2ffdd873b1f1c4c2f8261a
Daniel Patrício <ddmestrado23@gmail.com>
2023-11-22 20:02:13 +0000

    GPS iOS tracking 100% done without showing on emulator

--- FacialRecognitionPrototipe/ViewController.swift
+++ FacialRecognitionPrototipe/ViewController.swift
@@ -15,14 +15,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     private var locationManager:CLLocationManager?
     
-    private var latLngLabel:UILabel = {
+    /*private var latLngLabel:UILabel = {
         let label = UILabel()
         label.backgroundColor = .systemFill
         label.numberOfLines = 0
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 26)
         return label
-    } ()
+    } ()*/
     
     @IBOutlet var FacesCount: UILabel!
     
@@ -47,15 +47,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Do any additional setup after loading the view
         
         addCameraInput()
-        //showCameraFeed()
+        showCameraFeed()
         
         getCameraFrames()
         
         // Start capturing from the camera
         captureSession.startRunning()
         
-        latLngLabel.frame = CGRect(x: 20, y: view.bounds.height / 2 - 50, width: view.bounds.width - 40, height: 100)
-        view.addSubview(latLngLabel)
+        /*latLngLabel.frame = CGRect(x: 20, y: view.bounds.height / 2 - 50, width: view.bounds.width - 40, height: 100)
+        view.addSubview(latLngLabel)*/
         
         locationManager = CLLocationManager()
         locationManager?.requestAlwaysAuthorization()
@@ -66,7 +66,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
-            latLngLabel.text = "Lat: \(location.coordinate.latitude) \nLng: \(location.coordinate.longitude)"
+            /*latLngLabel.text =  "Lat: \(location.coordinate.latitude) \nLng: \(location.coordinate.longitude)"*/
         }
     }
     
