28f03958f8a49163914f678575e4cf4248ff1171
Imeguras <imeguras@cock.li>
2024-01-03 18:15:29 +0000

    Teste1

--- FacialRecognitionPrototipe/Services/LocationService.swift
+++ FacialRecognitionPrototipe/Services/LocationService.swift
@@ -14,9 +14,17 @@ extension LocationService {
         let search = MKLocalSearch(request: request)
 
         search.start { response, error in
+
             if let response = response {
                 completion(response.mapItems)
             }
+			let latitude = response?.boundingRegion.center.latitude
+			let longitude = response?.boundingRegion.center.longitude
+			
+			let annotation = MKPointAnnotation()
+			annotation.title = query
+			annotation.coordinate = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
+			self.mapView.addAnnotation(annotation)
         }
     }
 }
